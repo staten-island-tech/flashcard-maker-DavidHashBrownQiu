e30b2b5315393827341cb3b0f22f70c41b451265
DavidHashBrownQiu <longhutch@gmail.com>
2025-03-27 10:15:10 -0400

    gettingdownteacheranddarwenmadeadarkmarketforsomereasonaaaaaaaaaa

--- flashcards.py
+++ flashcards.py
@@ -12,17 +12,35 @@ def flashcards():
 
         def dic(self):
             return{self.question: self.answer}
+        
+        def text(self):
+            (f"{self.question}, {self.answer}")
+        
 
-    if ask.lower() == "teacher":
-        question = input("What's your question? ")
-        answer = input("What's your answer?")
-        flashcards = []
-        flash1 = Flashcard(question, answer)
-        print(flash1.dic())
+    while True:
+        if ask.lower() == "teacher":
+            question = input("What's your question? ")
+            answer = input("What's your answer? ")
+            flashcards = []
+            flash1 = Flashcard(question, answer)
+            flash2 = flash1.text()
+            flash1.append(flashcards)
+
+            question = input("Continue (Yes/No): ").lower()
+
+            if question == "yes":
+                continue
+
+            flash2 = [flash1.dic() for flashcard in flashcards]
+            print(f"here are all the questions: {flash2}")
             
+            with open("flashcards.json", "w") as file:
+                json.dump(flash2, file, indent=4)
+
+            if question == "no":
+                break
+
+
             
+flashcards()    
 
-    
-
-"""with open("flashcards.json", "w") as file:
-    json.dump()"""
